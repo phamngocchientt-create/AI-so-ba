52f0af1d5bfa1668cbca0f044487e41902474a6a
phamngocchientt-create <phamngocchientt@gmail.com>
2025-11-28 07:00:15 +0700

    Update app.py

--- chat_bot_so_10/app.py
+++ chat_bot_so_10/app.py
@@ -8,7 +8,7 @@ import io # Cần thiết cho việc xử lý file ảnh
 # 📌 BƯỚC 1: DÁN DANH SÁCH FILE ID CỦA BẠN VÀO ĐÂY
 # ==================================================
 # DÁN fileId THỰC TẾ CỦA BẠN
-LIST_FILES = ['files/qcp47mbnibs7', 'files/zz8witkvuw9h', 'files/bt4cvwky75qt']
+LIST_FILES = ['files/lne65mpap3l8', 'files/l4o41euv06nu', 'files/58a2u0wk4p7v']
 # ==================================================
 
 st.set_page_config(page_title="Gia sư Hóa học THCS", layout="wide")
@@ -178,6 +178,7 @@ if prompt := st.chat_input("Nhập câu hỏi..."):
                 st.session_state.messages.append({"role": "assistant", "content": response.text})
             except Exception as e:
                 st.error(f"Lỗi: {e}")
+
 
 
 
