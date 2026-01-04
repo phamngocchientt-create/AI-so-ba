8012aa1dae2e6c6405c49714c1cff0af5edbeea9
phamngocchientt-create <phamngocchientt@gmail.com>
2026-01-04 21:09:51 +0700

    Update app.py

--- chat_bot_so_10/app.py
+++ chat_bot_so_10/app.py
@@ -8,7 +8,7 @@ import json
 # ==================================================
 # 📌 BƯỚC 1: DANH SÁCH FILE ID (Giữ nguyên)
 # ==================================================
-LIST_FILES = ['files/hoqcm6xq9j76', 'files/ufpisglugupv']
+LIST_FILES = ['files/93m42nosu7th', 'files/jkdcxa0ebsgn']
 # ==================================================
 
 STORAGE_FILE = "missing_questions.json"
@@ -226,6 +226,7 @@ if prompt:
                         
                 except Exception as e:
                     st.error(f"Lỗi: {e}")
+
 
 
 
