f061dc9548297de00626477e88a4685a238d9ada
phamngocchientt-create <phamngocchientt@gmail.com>
2026-05-24 19:02:09 +0700

    Update app.py

--- chat_bot_so_10/app.py
+++ chat_bot_so_10/app.py
@@ -8,7 +8,7 @@ import json
 # ==================================================
 # 📌 CẤU HÌNH HỆ THỐNG
 # ==================================================
-LIST_FILES = ['files/2omlivob1hz3', 'files/dtqptbo4w0tf']
+LIST_FILES = ['files/t3jir4bd1esg', 'files/41xvkpgs5byg']
 STORAGE_FILE = "missing_questions.json"
 HISTORY_FILE = "chat_history.json"  # File mới để lưu lịch sử chat
 ERROR_MESSAGE_TAG = "[MISSING_DOC]"
